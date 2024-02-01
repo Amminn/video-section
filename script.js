57ae2e914dfa4dc401bf96bc19157ede11d4b90c
Amminn <vrb.nnn@gmail.com>
2024-02-02 00:28:28 +0100

    update: add comments

--- script.js
+++ script.js
@@ -3,26 +3,30 @@ let videoWrapper = document.getElementById("videoWrapper");
 let videoOpenButton = document.getElementById("v-plchldr")
 let videoCloseButton = document.getElementById("videoCloseButton");
 
+// Add event listener for the video open button
+// When button is clicked, display the video wrapper then play the video
 videoOpenButton.addEventListener("click", () => {
-  console.log('open')
   videoWrapper.style.display = "block";
   playVideo();
 })
 
+// Add event listener for the video close button
+// When button is clicked, stop the video and hide the video wrapper
 videoCloseButton.addEventListener("click", () => {
-  console.log('close')
   stopVideo();
 })
 
+// Function to play the video
+// It updates the videoFrame src to include autoplay query parameter
 function playVideo() {
   var src = videoFrame.src;
   videoFrame.src = src + "?autoplay=1";
 }
 
+// Function to stop the video
+// It hides the video wrapper and removes the autoplay query parameter from videoFrame src to stop it
 function stopVideo() {
-  console.log(videoWrapper)
-  videoWrapper.style.display = "none";
   var src = videoFrame.src;
   videoFrame.src = src.replace("?autoplay=1", "");
-  // videoFrame.style.display = "none";
+  videoWrapper.style.display = "none";
 }
