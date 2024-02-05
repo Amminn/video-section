a17d241bce12a3374b0dced5cabd7d3880f3e71e
Amminn <vrb.nnn@gmail.com>
2024-02-05 15:56:52 +0100

    feat: implement vimeo autoplay

--- script.js
+++ script.js
@@ -1,32 +1,48 @@
 let videoFrame = document.getElementById("videoFrame");
 let videoWrapper = document.getElementById("videoWrapper");
-let videoOpenButton = document.getElementById("v-plchldr")
+let videoOpenButton = document.getElementById("v-plchldr");
 let videoCloseButton = document.getElementById("videoCloseButton");
+let vimeoPlayer;
 
 // Add event listener for the video open button
 // When button is clicked, display the video wrapper then play the video
 videoOpenButton.addEventListener("click", () => {
   videoWrapper.style.display = "block";
   playVideo();
-})
+});
 
 // Add event listener for the video close button
 // When button is clicked, stop the video and hide the video wrapper
 videoCloseButton.addEventListener("click", () => {
   stopVideo();
-})
+});
+
+// Function to initialize the Vimeo player
+function initializePlayer() {
+  vimeoPlayer = new Vimeo.Player(videoFrame);
+
+  // You can add event listeners for player events here if needed
+  // Example: vimeoPlayer.on('ended', function() { /* Do something */ });
+}
 
 // Function to play the video
-// It updates the videoFrame src to include autoplay query parameter
-function playVideo() {
-  var src = videoFrame.src;
-  videoFrame.src = src + "?autoplay=1";
+async function playVideo() {
+  if (!vimeoPlayer) {
+    initializePlayer();
+  }
+
+  // Play the video
+  try {
+    await vimeoPlayer.play();
+  } catch (error) {
+    console.error("Error playing Vimeo video:", error);
+  }
 }
 
 // Function to stop the video
-// It hides the video wrapper and removes the autoplay query parameter from videoFrame src to stop it
 function stopVideo() {
-  var src = videoFrame.src;
-  videoFrame.src = src.replace("?autoplay=1", "");
+  if (vimeoPlayer) {
+    vimeoPlayer.unload();
+  }
   videoWrapper.style.display = "none";
 }
